a3da4a7eab52f276dac4ec3d4722d3dbd732cc4d
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-15 15:43:29 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,7 +25,7 @@ const WETH_ONLY: ChainTokenList = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
   ...WETH_ONLY, 
-  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], CFT],
+  [ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET]],
 }
 
 /**
